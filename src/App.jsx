dc84631b29e2758cadfd54fae4c22486e991546c
SarthakRay26 <126063026+SarthakRay26@users.noreply.github.com>
2024-03-09 20:51:06 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import ClockHeading from "./components/ClockHeading";
+import ClockSlogan from "./components/ClockSlogan";
+import ClockTime from "./components/ClockTime";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  
+
+  return (
+    
+    <center>
+      <ClockHeading />
+      <ClockSlogan />
+      <ClockTime />
+
+    </center>
+    
+  );
+}
+
+export default App;
